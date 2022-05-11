9be58afc2951ee9bf0b3697e05b3a7c97d5464c4
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2022-05-11 13:51:05 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,20 +1,2 @@
-import java.util.*;
-class Leap
-{
-    public static void main(String args[])
-    {
-       int year;
-     Scanner in=new Scanner(System.in);
-     System.out.println("Enter the value of year");
-       year=in.nextInt();
-      if((((year % 4 == 0) && (year % 100 != 0)) ||
-            (year % 400 == 0)))
-       {
-   System.out.println("yearis a leap year");
-        }
-    else
-      {
-   System.out.println("year is not a leap year");
-     }
- }
-}
+for i in range(1,10):
+    print(i)
